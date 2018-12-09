be17aef2e0a87b0756ec88635182413ae21e0778
Dan Burnside <dburnside@stmtires.com@Dans-MBP.attlocal.net>
2018-12-08 21:55:35 -0600

    updated app routing with main route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainCalendarViewComponent } from './main-calendar-view/main-calendar-view.component';
-import { IndividualCalendarViewComponent } from './individual-calendar-view/individual-calendar-view.component';
 
 const routes: Routes = [
   { path: '', component: MainCalendarViewComponent },
-  { path: 'calendar-view', component: IndividualCalendarViewComponent },
 ];
 
 @NgModule({
