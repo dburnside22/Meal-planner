328888fe281a7af7e669fb9431a8e12764cb42f1
Dan Burnside <dburnside@stmtires.com@Dans-MBP.attlocal.net>
2018-12-11 00:04:47 -0600

    added new meal picker route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainCalendarViewComponent } from './main-calendar-view/main-calendar-view.component';
+import { MealPickerComponent } from './meal-picker/meal-picker.component';
 
 const routes: Routes = [
   { path: '', component: MainCalendarViewComponent },
+  { path: 'meal-picker', component: MealPickerComponent },
 ];
 
 @NgModule({
