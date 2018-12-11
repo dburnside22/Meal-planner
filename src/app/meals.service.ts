64ae9b54a8ed4fc4fce37d0779be4860849af3fa
Dan Burnside <dburnside@stmtires.com@Dans-MBP.attlocal.net>
2018-12-11 00:05:44 -0600

    updated meal service to incorperate new get mealsByCatagory function

--- src/app/meals.service.ts
+++ src/app/meals.service.ts
@@ -8,16 +8,20 @@ import { Meal } from './meal.interface';
 })
 export class MealsService {
 
-  mealsUrl = 'http://127.0.0.1:8000/meals/';
+  mealsUrl = 'http://127.0.0.1:8000/';
 
   constructor(private http: HttpClient) { }
 
   updateDay(meal: Meal): Observable<any> {
-    return this.http.put(`${this.mealsUrl}${meal.id}/`, meal);
+    return this.http.put(`${this.mealsUrl}meals/${meal.id}/`, meal);
   }
 
   listAllMeals(): Observable<any> {
-    return this.http.get(this.mealsUrl);
+    return this.http.get(`${this.mealsUrl}meals`);
+  }
+
+  getMealsByCatagory(foodCatagory: string): Observable<any> {
+    return this.http.get(`${this.mealsUrl}${foodCatagory}`);
   }
 
 }
